d3b47727705249c8fa3a897de8d1c5e278bb170f
Biruk-N <biruk.rcd.1912@gmail.com>
2022-03-23 01:20:34 +0300

    init

---
+++ store_list/models.py
@@ -0,0 +1,86 @@
+import collections
+from http.client import PAYMENT_REQUIRED
+from itertools import product
+from tkinter import CASCADE
+from django.db import models
+
+# Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    
+    # product = models.ForeignKey(Product, on_delete=CASCADE)
+    # this should be defined in product class
+class Product(models.Model):
+    # model field types
+    # id created automatically created by django
+    title  = models.CharField(max_length=255)
+    description = models.TextField()
+    # let say max price is 9999.99
+    price = models.DecimalField(max_digits=6, decimal_places=2)  
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    # if collection deleted but not product
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+class Order(models.Models):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    
+    PAYMENT_STATUS_CHOICES =  [
+        (PAYMENT_STATUS_PENDING, 'pending'),
+        (PAYMENT_STATUS_COMPLETE, 'complete'),
+        (PAYMENT_STATUS_FAILED, 'failed')
+    ]
+
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status =models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    # price defined again because the price should change over time
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    # customer = models.OneToOneRel(Customer, on_delete=models.CASCADE, primary_key=True)
+    # because we don't want to create id for address that cause many to many relation
+    # if it has many to many relation
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=CASCADE)
+    product =models.ForeignKey(Product, on_delete=CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    
